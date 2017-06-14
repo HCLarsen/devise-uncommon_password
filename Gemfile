6543948594348d76bf3e68725f1926a2088d7cbc
Chris Larsen <clarsenipod@gmail.com>
2017-06-14 14:51:47 -0400

    Add tests

--- Gemfile
+++ Gemfile
@@ -12,3 +12,4 @@ gemspec
 
 # To use a debugger
 # gem 'byebug', group: [:development, :test]
+gem "devise", "~> 4.3"
--- test/devise/uncommon_password_test.rb
+++ test/devise/uncommon_password_test.rb
@@ -4,4 +4,17 @@ class Devise::UncommonPassword::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, Devise::UncommonPassword
   end
+
+  test "should deny validation for a common password" do
+    msg = "Chosen password is common and easy for hackers to guess. Please choose a unique password."
+    password = common_passwords.first
+    user = User.create email:"example@example.org", password: password, password_confirmation: password
+    assert_not user.valid?, "User with common password shoud not be valid."
+  end
+
+  test "should accept validation for an uncomm password" do
+    password = "fddkasnsdddghjt"
+    user = User.create email:"example@example.org", password: password, password_confirmation: password
+    assert user.valid?, "User with uncommon password should be valid."
+  end
 end
