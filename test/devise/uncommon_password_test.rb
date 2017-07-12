fe527c65a596d417b7d145171aac749e2d67eb55
Chris Larsen <clarsenipod@gmail.com>
2017-07-12 14:00:07 -0400

    Fix typo

--- test/devise/uncommon_password_test.rb
+++ test/devise/uncommon_password_test.rb
@@ -14,7 +14,7 @@ class Devise::UncommonPassword::Test < ActiveSupport::TestCase
     password = passwords.first.upcase
     user = User.create email:"example@example.org", password: password, password_confirmation: password
     assert_not user.valid?, "Uppercase common passwords shoud not be valid."
-    assert_equal ["is a very common passwords. Please choose something harder to guess."], user.errors[:password]
+    assert_equal ["is a very common password. Please choose something harder to guess."], user.errors[:password]
   end
 
   test "should accept validation for an uncommon password" do
