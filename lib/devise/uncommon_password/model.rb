44b4f4aa2907f4ed5a87f2a4c6ce4c81c24437b8
Igor Springer <springerigor@gmail.com>
2024-04-24 09:01:34 +0200

    [IS-4171] fix: ignore nil passwords
    When there are suspicious requests to the "create user" endpoint, `password` param is not always set and the module explodes due to `undefined method `downcase' for nil:NilClass` error.
    
    As there is another module for validating password length, this one should check password against the list of commond password only if the password is present
    
    Password is missing when an incorrect requests to the user registration endpoint are made, example:
    
    ```bash
    curl -v 'http://localhost:3002/users' -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' --data ''
    ```

--- lib/devise/uncommon_password/model.rb
+++ lib/devise/uncommon_password/model.rb
@@ -30,7 +30,7 @@ module Devise
       private
 
       def not_common_password
-        if Devise::Models::UncommonPassword.common_passwords.include? password.downcase
+        if Devise::Models::UncommonPassword.common_passwords.include? password&.downcase
           errors.add(:password, :common_password)
         end
       end
